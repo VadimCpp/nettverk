76556751ec03aa79f23370ff91cf0addc4985197
Vadym Kaninskyi <vadm.cpp@gmail.com>
2024-10-16 14:53:41 +0200

    Edu: add big text

--- lib/src/sample_feature/about_us_view.dart
+++ lib/src/sample_feature/about_us_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AboutUsView extends StatelessWidget {
   const AboutUsView({super.key});
@@ -9,13 +10,13 @@ class AboutUsView extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-            padding: const EdgeInsets.only(top: 20, bottom: 20),
-            child: Text(
-              'Please check your email for the code',
-              textAlign: TextAlign.start,
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
+          padding: const EdgeInsets.only(top: 20, bottom: 20),
+          child: Text(
+            AppLocalizations.of(context)!.aboutNettverk,
+            textAlign: TextAlign.start,
+            style: Theme.of(context).textTheme.bodyMedium,
           ),
+        ),
       ],
     );
   }
