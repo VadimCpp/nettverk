1c1585a20ea6bc47d71a4ac67b2486403be364ec
Vadym Kaninskyi <vadm.cpp@gmail.com>
2024-10-16 12:42:33 +0200

    Edu: add splash screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
-/// The Widget that configures your application.
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({
     super.key,
     required this.settingsController,
@@ -17,13 +17,42 @@ class MyApp extends StatelessWidget {
   final SettingsController settingsController;
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+/// The Widget that configures your application.
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Run the method after the first frame is rendered.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      onComponentFullyLoaded();
+    });
+  }
+
+  void onComponentFullyLoaded() {
+    // This method is called once the component is fully loaded.
+    // You can perform any actions you need here.
+    print("Component is fully loaded!");
+    // Example: You can interact with settingsController here.
+    widget.settingsController.loadSettings();
+
+    // Set a timeout to remove the splash screen after 1 second
+    Future.delayed(const Duration(seconds: 1), () {
+      // whenever your initialization is completed, remove the splash screen:
+      FlutterNativeSplash.remove();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     // Glue the SettingsController to the MaterialApp.
     //
     // The ListenableBuilder Widget listens to the SettingsController for changes.
     // Whenever the user updates their settings, the MaterialApp is rebuilt.
     return ListenableBuilder(
-      listenable: settingsController,
+      listenable: widget.settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
           // Providing a restorationScopeId allows the Navigator built by the
@@ -63,7 +92,7 @@ class MyApp extends StatelessWidget {
           // SettingsController to display the correct theme.
           theme: ThemeData(),
           darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
+          themeMode: widget.settingsController.themeMode,
 
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
@@ -73,7 +102,7 @@ class MyApp extends StatelessWidget {
               builder: (BuildContext context) {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
-                    return SettingsView(controller: settingsController);
+                    return SettingsView(controller: widget.settingsController);
                   case SampleItemDetailsView.routeName:
                     return const SampleItemDetailsView();
                   case SampleItemListView.routeName:
