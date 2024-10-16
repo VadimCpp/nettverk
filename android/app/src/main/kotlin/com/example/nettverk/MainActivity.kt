5bc3a10742b3664e86e786c630626db24fd250b3
Vadym Kaninskyi <vadm.cpp@gmail.com>
2024-10-16 15:06:21 +0200

    Edu: update App ID

--- android/app/src/main/kotlin/com/example/nettverk/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nettverk/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nettverk
+package no.tekzence.nettverk
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/src/sample_feature/about_us_view.dart
+++ lib/src/sample_feature/about_us_view.dart
@@ -7,15 +7,24 @@ class AboutUsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
+        const SizedBox(height: 20),
         Padding(
-          padding: const EdgeInsets.only(top: 20, bottom: 20),
+          padding: const EdgeInsets.only(left: 20, right: 20),
           child: Text(
             AppLocalizations.of(context)!.aboutNettverk,
             textAlign: TextAlign.start,
             style: Theme.of(context).textTheme.bodyMedium,
           ),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            // Navigate back to home screen
+            Navigator.of(context).pop();
+          },
+          child: const Text('Back'),
         ),
       ],
     );
