efd15be8c1c0f3a126bfe9c59c55b4ebada38bff
Vadym Kaninskyi <vadm.cpp@gmail.com>
2024-10-21 13:36:31 +0200

    Edu: use Consumer for performance optimization

--- lib/src/screens/settings_screen.dart
+++ lib/src/screens/settings_screen.dart
@@ -13,39 +13,41 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final settingsController = Provider.of<SettingsController>(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        // Glue the SettingsController to the theme selection DropdownButton.
-        //
-        // When a user selects a theme from the dropdown list, the
-        // SettingsController is updated, which rebuilds the MaterialApp.
-        child: DropdownButton<ThemeMode>(
-          // Read the selected themeMode from the controller
-          value: settingsController.themeMode,
-          // Call the updateThemeMode method any time the user selects a theme.
-          onChanged: settingsController.updateThemeMode,
-          items: const [
-            DropdownMenuItem(
-              value: ThemeMode.system,
-              child: Text('System Theme'),
+    return Consumer<SettingsController>(
+      builder: (context, settingsController, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Settings'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            // Glue the SettingsController to the theme selection DropdownButton.
+            //
+            // When a user selects a theme from the dropdown list, the
+            // SettingsController is updated, which rebuilds the MaterialApp.
+            child: DropdownButton<ThemeMode>(
+              // Read the selected themeMode from the controller
+              value: settingsController.themeMode,
+              // Call the updateThemeMode method any time the user selects a theme.
+              onChanged: settingsController.updateThemeMode,
+              items: const [
+                DropdownMenuItem(
+                  value: ThemeMode.system,
+                  child: Text('System Theme'),
+                ),
+                DropdownMenuItem(
+                  value: ThemeMode.light,
+                  child: Text('Light Theme'),
+                ),
+                DropdownMenuItem(
+                  value: ThemeMode.dark,
+                  child: Text('Dark Theme'),
+                )
+              ],
             ),
-            DropdownMenuItem(
-              value: ThemeMode.light,
-              child: Text('Light Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.dark,
-              child: Text('Dark Theme'),
-            )
-          ],
-        ),
-      ),
+          )
+        );
+      },
     );
   }
 }
