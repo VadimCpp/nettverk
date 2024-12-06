6f0ac26c5f93d290686de191cbde6505eaba94a1
Vadym Kaninskyi <vadm.cpp@gmail.com>
2024-12-06 10:12:18 +0100

    Edu: remove list item and consumer

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -3,10 +3,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webfeed_revised/webfeed_revised.dart';
-import 'package:nettverk/src/controllers/index.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -78,183 +76,152 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ItemsController>(
-      builder: (context, itemsController, child) {
-        final localizedItems = itemsController.items(context);
-
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(AppLocalizations.of(context)!.appTitle),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed: () {
-                  context.push('/settings');
-                },
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.appTitle),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              context.push('/settings');
+            },
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                // Welcome Message
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-                  child: Center(
-                    child: Text(
-                      AppLocalizations.of(context)!.welcomeMessage,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Welcome Message
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+              child: Center(
+                child: Text(
+                  AppLocalizations.of(context)!.welcomeMessage,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.left,
                 ),
-                // Instruction text
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-                  child: Center(
-                    child: Text(
-                      AppLocalizations.of(context)!.chooseSection,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
+              ),
+            ),
+            // Instruction text
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              child: Center(
+                child: Text(
+                  AppLocalizations.of(context)!.chooseSection,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.left,
                 ),
-                // Quick Links
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Wrap(
-                    spacing: 10,
-                    runSpacing: 10,
-                    alignment: WrapAlignment.center,
-                    children: [
-                      ElevatedButton.icon(
-                        icon: Icon(Icons.chat),
-                        label: Text(AppLocalizations.of(context)!.chats),
-                        onPressed: () {
-                          context.push('/chats');
-                        },
-                      ),
-                      ElevatedButton.icon(
-                        icon: Icon(Icons.assistant),
-                        label: Text(AppLocalizations.of(context)!.botassistant),
-                        onPressed: () {
-                          context.push('/bot');
-                        },
-                      ),
-                      ElevatedButton.icon(
-                        icon: Icon(Icons.school),
-                        label: Text(AppLocalizations.of(context)!.education),
-                        onPressed: () {
-                          context.push('/edu');
-                        },
-                      ),
-                      ElevatedButton.icon(
-                        icon: Icon(Icons.info),
-                        label: Text(AppLocalizations.of(context)!.aboutUs),
-                        onPressed: () {
-                          context.push('/about');
-                        },
-                      ),
-                      // Add more quick links as needed
-                    ],
+              ),
+            ),
+            // Quick Links
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                alignment: WrapAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.chat),
+                    label: Text(AppLocalizations.of(context)!.chats),
+                    onPressed: () {
+                      context.push('/chats');
+                    },
                   ),
-                ),
-                SizedBox(height: 20),
-                // Latest News Feed
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      AppLocalizations.of(context)!.latestNews,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.assistant),
+                    label: Text(AppLocalizations.of(context)!.botassistant),
+                    onPressed: () {
+                      context.push('/bot');
+                    },
                   ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.school),
+                    label: Text(AppLocalizations.of(context)!.education),
+                    onPressed: () {
+                      context.push('/edu');
+                    },
+                  ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.info),
+                    label: Text(AppLocalizations.of(context)!.aboutUs),
+                    onPressed: () {
+                      context.push('/about');
+                    },
+                  ),
+                  // Add more quick links as needed
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            // Latest News Feed
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  AppLocalizations.of(context)!.latestNews,
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 5.0, right: 5.0, bottom: 20.0),
-                  child: FutureBuilder<RssFeed>(
-                    future: _feed,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        final items = snapshot.data!.items!;
-                        return ListView.builder(
-                          itemCount: items.length,
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemBuilder: (context, index) {
-                            final item = items[index];
-                            return ListTile(
-                              title: Text(item.title ?? ''),
-                              subtitle: Text(
-                                item.pubDate != null 
-                                  ? DateFormat.yMMMMd(Localizations.localeOf(context).toString())
-                                    .format(item.pubDate!)
-                                  : '',
-                              ),
-                              // Text(item.pubDate?.toLocal().toString() ?? ''),
-                              onTap: () async {
-                                if (item.link != null) {
-                                  final url = Uri.parse(item.link!);
-                                  if (await canLaunchUrl(url)) {
-                                    await launchUrl(url);
-                                  } else {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(content: Text(AppLocalizations.of(context)!.cannotLaunchUrl)),
-                                    );
-                                  }
-                                }
-                              },
-                            );
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 5.0, right: 5.0, bottom: 20.0),
+              child: FutureBuilder<RssFeed>(
+                future: _feed,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final items = snapshot.data!.items!;
+                    return ListView.builder(
+                      itemCount: items.length,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        final item = items[index];
+                        return ListTile(
+                          title: Text(item.title ?? ''),
+                          subtitle: Text(
+                            item.pubDate != null 
+                              ? DateFormat.yMMMMd(Localizations.localeOf(context).toString())
+                                .format(item.pubDate!)
+                              : '',
+                          ),
+                          // Text(item.pubDate?.toLocal().toString() ?? ''),
+                          onTap: () async {
+                            if (item.link != null) {
+                              final url = Uri.parse(item.link!);
+                              if (await canLaunchUrl(url)) {
+                                await launchUrl(url);
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text(AppLocalizations.of(context)!.cannotLaunchUrl)),
+                                );
+                              }
+                            }
                           },
                         );
-                      } else if (snapshot.hasError) {
-                        return Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Text(
-                            AppLocalizations.of(context)!.feedLoadError,
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                        );
-                      } else {
-                        return Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: ListView.builder(
-                    itemCount: localizedItems.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (BuildContext context, int index) {
-                      final item = localizedItems[index];
-                      return ListTile(
-                        title: Text(
-                          item.title,
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        leading: CircleAvatar(
-                          foregroundImage: AssetImage(item.logo),
-                        ),
-                        onTap: () {
-                          context.push("/chat", extra: item);
-                        },
-                      );
-                    },
-                  ),
-                ),
-
-              ],
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        AppLocalizations.of(context)!.feedLoadError,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    );
+                  } else {
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
